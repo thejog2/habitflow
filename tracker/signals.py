927e1f9e47b9147c4cde47d43daee53cff6769bd
TeasDale <jordanjohnteasdale@gmail.com>
2026-03-04 10:42:38 +0000

    added admin functionality to the nav bar for superusers

--- tracker/signals.py
+++ tracker/signals.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-@receiver(post_save, User)
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-@receiver(post_save, User)
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
